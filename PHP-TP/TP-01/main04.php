4a9360e36379e4723da73b1b7c0013fab95d13fe
alireza-sadi <116587064+alireza-sadi@users.noreply.github.com>
2023-04-26 12:29:53 +0330

    Delete PHP-TP/TP-01 directory

--- PHP-TP/TP-01/main04.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$str="you";
-
-printf("How are %s today?\n",$str);
-
-printf("Price: %d %o %x %X %b %c",1000,1000,1000,1000,1000,65);
--- PHP-TP/TP-01/main10.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-for($i=0;$i<10;$i++)
-    echo str_shuffle("Mohsen")."\n";
-
-echo "\n"."\n".strrev("Hello");
